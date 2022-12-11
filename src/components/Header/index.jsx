15f191d5255773bd671e94f5b064e5f9a3345639
Vinicius J. F <www.vini84200@hotmail.com>
2022-12-11 15:40:28 -0300

    Mudei o titulo da pagina

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,7 @@ import styles from './style.module.css'
 import React from 'react'
 export default function Header(){
     return(
-        < div className={styles.container}>
+        <div className={styles.container}>
             <ul>
                 <li className={styles.itemLista}>HOME</li>
                 <li className={styles.itemLista}>SOBRE</li>

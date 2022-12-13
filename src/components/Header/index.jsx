94dc9ddc77540b8a4bdc795c3bb2d11517feec42
leoheisler <72279482+leoheisler@users.noreply.github.com>
2022-12-13 20:46:25 -0300

    int header anim

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,6 +4,7 @@ export default function Header(){
     return(
         <div className={styles.container}>
             <ul>
+                
                 <li className={styles.itemLista}><a href="">HOME</a></li>
                 <li className={styles.itemLista}><a href="#sobre a seleção">SOBRE</a></li>
                 <li className={styles.itemLista}><a href="#jogadores">JOGADORES</a></li>

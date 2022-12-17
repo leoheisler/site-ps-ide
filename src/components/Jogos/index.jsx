1a8f1a7283247a8d7f19b55bf6587bfac751c0ee
Paulo Cauê Krüger Costa <62337888+paulokrg@users.noreply.github.com>
2022-12-17 02:47:36 -0300

    Versão 1 finalizada

--- src/components/Jogos/index.jsx
+++ src/components/Jogos/index.jsx
@@ -1,8 +1,52 @@
-import React from 'react';
-import './footer.module.css';
+import React from "react";
+import styles from "./jogos.module.css";
 
-export default function () {
-    return <div>
-        JOGOS HERE
-    </div>
+export default function Jogos() {
+	return (
+		<div class={styles.container}>
+			<h1 class={styles.title}>Próximos Jogos</h1>
+
+			<div class={styles.box}>
+				<div class={styles.box_top}>
+					<img src="jogos/brasil.png" alt="Time 1" />
+					<img class={styles.x_image} src="jogos/x.png" alt="X" />
+					<img src="jogos/japao.png" alt="Time 2" />
+				</div>
+
+				<div class={styles.box_bottom}>
+					<p>Quinta-feira</p>
+					<p>02/11</p>
+					<p>07:20</p>
+				</div>
+			</div>
+
+			<div class={styles.box}>
+				<div class={styles.box_top}>
+					<img src="jogos/brasil.png" alt="Time 1" />
+					<img class={styles.x_image} src="jogos/x.png" alt="X" />
+					<img src="jogos/coreia.png" alt="Time 2" />
+				</div>
+
+				<div class={styles.box_bottom}>
+					<p>Sexta-feira</p>
+					<p>07/11</p>
+					<p>08:00</p>
+				</div>
+			</div>
+
+			<div class={styles.box}>
+				<div class={styles.box_top}>
+					<img src="jogos/brasil.png" alt="Time 1" />
+					<img class={styles.x_image} src="jogos/x.png" alt="X" />
+					<img src="jogos/ghana.png" alt="Time 2" />
+				</div>
+
+				<div class={styles.box_bottom}>
+					<p>Sábado</p>
+					<p>11/11</p>
+					<p>20:30</p>
+				</div>
+			</div>
+		</div>
+	);
 }
